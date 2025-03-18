3eb32af087692df83af88d9714b5cf81e1c92ff1
Guinn <eguinjonathan08@gmail.com>
2025-03-18 17:08:16 +0700

    feat: add bodyparser to parse json

--- myApp.js
+++ myApp.js
@@ -1,22 +1,26 @@
 require('dotenv').config();
+const bodyParser = require('body-parser');
 let express = require('express');
 let app = express();
 
 console.log("Hello World");
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+
 app.use("/public", express.static(__dirname + "/public"));
 
-app.use(function middleware(req, res, next) {
+app.use(function middleware(req, _, next) {
   var string = req.method + " " + req.path + " - " + req.ip;
   console.log(string);
   next();
 });
 
-app.get("/", (req, res) => {
+app.get("/", (_, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-app.get("/json", (req, res) => {
+app.get("/json", (_, res) => {
   if (process.env.MESSAGE_STYLE === "uppercase") {
     res.json({
       message: "HELLO JSON"
@@ -28,7 +32,7 @@ app.get("/json", (req, res) => {
   }
 });
 
-app.get("/now", function middleware(req, res, next) {
+app.get("/now", function middleware(req, _, next) {
   req.time = new Date().toString();
   next();
 }, (req, res) => {
