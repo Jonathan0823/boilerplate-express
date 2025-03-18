659578d8e66a1bec766d319ec318bf10618ed68a
Guinn <eguinjonathan08@gmail.com>
2025-03-18 07:27:10 +0700

    add /now routes

--- myApp.js
+++ myApp.js
@@ -28,6 +28,15 @@ app.get("/json", (req, res) => {
   }
 });
 
+app.get("/now", function middleware(req, res, next) {
+  req.time = new Date().toString();
+  next();
+}, (req, res) => {
+  res.json({
+    time: req.time
+  });
+});
+
 
 
 
